d6c1564dcbc316a9ee73c05c2c8b3208b6512389
이진실 <ljsil1204@naver.com>
2022-02-15 17:16:04 +0900

    이전 화면에서 보낸 문장을 받아서 활용

--- app/src/main/java/com/neppplus/intent_20220215/ViewMessageActivity.kt
+++ app/src/main/java/com/neppplus/intent_20220215/ViewMessageActivity.kt
@@ -2,10 +2,20 @@ package com.neppplus.intent_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
+
+//        화면이 시작되면 바로 문구 설정
+
+//        이전화면에서 ("message" 이름표 붙여서) 첨부한 문구받기
+
+        val message = intent.getStringExtra("message")
+
+        txtMessage.text = message
+
     }
 }
